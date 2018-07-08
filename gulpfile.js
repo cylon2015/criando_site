d464d97b674209164f69cb358cd876dfab1ce495
cylon <cylon2015@gmail.com>
2018-07-08 14:54:24 -0300

    se

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+gulp.task('sass', function () {
+	return sass('sass/*.sass').pipe(gulp.dest('css'))
+});
+gulp.task('default',function () {
+	
+});
